9311236a3c06520113e2653ad20aaaae0eb6b26f
kittenyost <kathyiyost@gmail.com>
2023-10-17 19:17:52 -0700

    Add project files.

---
+++ StringMaker_Yost/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringMaker_Yost;
+
+StringManager sm = new StringManager();
+Console.WriteLine($"Reversed: {sm.Reverse("Sunbeam Tiger")}");
+Console.WriteLine($"ToString: {sm.ToString()}");
+Console.WriteLine($"Reversed Preserve Case: {sm.Reverse("Sunbeam Tiger", true)}");
+Console.WriteLine($"Equals: {sm.Equals("Sunbeam Tiger")}");
+Console.WriteLine($"Symmetric: {sm.Symmetric("ABBA")}");
+Console.WriteLine($"Symmetric: {sm.Symmetric("ABA")}");
+Console.WriteLine($"Symmetric: {sm.Symmetric("ABba")}");
---
+++ StringMaker_Yost/StringManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker_Yost
+{
+    internal class StringManager
+    {
+        public string CurrentInput { get; set; } = "";
+
+        public string Reverse(string s)
+        {
+            CurrentInput = s;
+            string reversedStr = "";
+            Stack<char> myStack = new Stack<char>();
+            foreach (char t in s)
+            {
+                myStack.Push(t);
+            }
+            while (myStack.Count > 0)
+            {
+                reversedStr += myStack.Pop();
+            }
+            return reversedStr;
+        }
+
+        public string Reverse(string s, bool PreserveCaseLocation)
+        {
+            CurrentInput = s;
+            string reverseString = Reverse(s);
+            string resultString = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    resultString += char.ToUpper(reverseString[i]);
+                }
+                else
+                {
+                    resultString += char.ToLower(reverseString[i]);
+                }
+            }
+            return resultString;
+        }
+
+        public bool Symmetric(string s)
+        {
+            CurrentInput = s;
+            return s == Reverse(s);
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(CurrentInput))
+            {
+                return "Negative One";
+            }
+
+            int asciiTotal = 0;
+            for (int i = 0; i < CurrentInput.Length; i++)
+            {
+                asciiTotal += Convert.ToInt32(CurrentInput[i]);
+            }
+
+            string numberString = asciiTotal.ToString();
+
+            string resultString = "";
+            Dictionary<char, string> dictNumbers = new Dictionary<char, string>()
+            {
+                { '0', "Zero" },
+                { '1', "One" },
+                { '2', "Two" },
+                { '3', "Three" },
+                { '4', "Four" },
+                { '5', "Five" },
+                { '6', "Six" },
+                { '7', "Seven" },
+                { '8', "Eight" },
+                { '9', "Nine" }
+            };
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                resultString += dictNumbers[numberString[i]] + " ";
+            }
+
+            return resultString.Trim();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return CurrentInput.Equals(obj);
+        }
+    }
+}
